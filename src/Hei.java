9204b691b1b36a44c5d4fd41ce9b270ff6cf9d1b
Ivar2001 <iaoqi@student.han.nl>
2020-09-02 11:17:39 +0200

    Initial commit

---
+++ src/Hei.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import javax.swing.*;
+import java.util.Scanner;
+
+//OPDRACHT 1
+//A:
+//public class Hei {
+//    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//    }
+//}
+
+//B:
+public class Hei {
+    public static void main(String[] args) {
+        String message = "Hello World";
+        JOptionPane.showMessageDialog(new JFrame(), message, "Window", JOptionPane.ERROR);
+    }
+}
+
+//OPDRACHT 2
+//public class Hei {
+//    public static void main(String[] args) {
+//        System.out.println("Hello "+ args[0]);
+//    }
+//}
